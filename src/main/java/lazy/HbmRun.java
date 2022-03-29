7dbab72936e3c4532300aee968e952924ef440c7
meavv <c.c.c.@bk.ru>
2022-03-29 17:13:21 +0500

    0. HQL [#6874]

--- src/main/java/lazy/HbmRun.java
+++ src/main/java/lazy/HbmRun.java
@@ -1,18 +1,16 @@
 package lazy;
 
+
+import model.Candidate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import org.hibernate.query.Query;
 
 public class HbmRun {
     public static void main(String[] args) {
-
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         try {
@@ -20,29 +18,68 @@ public class HbmRun {
             Session session = sf.openSession();
             session.beginTransaction();
 
-            List<Brand> list = session.createQuery("from Brand").list();
+            /**
+             * Сохранение объектов
+             Candidate candidate1 = Candidate.of("Name1", "Exp1", 100);
+             Candidate candidate2 = Candidate.of("Name2", "Exp2", 5.55);
+             Candidate candidate3 = Candidate.of("Name3", "Exp3", 99.99);
+             session.save(candidate1);
+             session.save(candidate2);
+             session.save(candidate3);
+             */
 
-            for (Brand brand : list) {
-                for (Model model : brand.getModels()) {
-                    System.out.println(model);
-                }
-            }
+            /**
+             * Выборка по всем кандидатам
+             Query query = session.createQuery("from Candidate");
+             System.out.println(query.getResultList());
+             */
 
+            /**
+             * Поиск по id
+             Query query = session.createQuery("from Candidate where id = :cId");
+             query.setParameter("cId", 2);
+             System.out.println(query.uniqueResult());
+             */
 
-/**
-            Brand brand = Brand.of("Brand 1");
-            Model m1 = Model.of("Model 1", brand);
-            Model m2 = Model.of("Model 2", brand);
-            brand.getModels().add(m1);
-            brand.getModels().add(m2);
-            session.save(m1);
-            session.save(m2);
-            session.save(brand);
-*/
+            /**
+             * Поиск по name
+             Query query = session.createQuery("from Candidate where name = :cName");
+             query.setParameter("cName", "Name3");
+             System.out.println(query.uniqueResult());
+             */
+
+            /**
+             * Update через hibernate
+             Candidate candidate = session.get(Candidate.class, 1);
+             candidate.setName("newName");
+             session.save(candidate);
+             */
+
+            /**
+             * Update через HQL
+             session.createQuery("update Candidate c set c.name= :pName where c.id = :pid")
+             .setParameter("pName", "Name222")
+             .setParameter("pid", 2)
+             .executeUpdate();
+             */
+
+            /**
+             * Delete через HQL
+             session.createQuery("delete Candidate c where c.id = :pId")
+             .setParameter("pId", 3)
+             .executeUpdate();
+             */
+
+            /**
+             * Delete через Hibernate
+             Candidate s = session.load(Candidate.class, 2);
+             session.delete(s);
+             */
+
 
             session.getTransaction().commit();
             session.close();
-        }  catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
